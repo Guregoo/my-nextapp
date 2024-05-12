1909415736d79b25e3afc0d96edf3e71e50f3bcd
Guregooo <michrio02@gmail.com>
2024-05-13 00:25:03 +0800

    PostCard minor change

--- components/PostsCard.jsx
+++ components/PostsCard.jsx
@@ -7,9 +7,9 @@ export default function PostsCard({postData, title}) {
   <div className='flex flex-wrap gap-10'>
     <h2>{title}</h2>
       {postData.map(({ slug, metadata }) => (
-        <div key={slug} className='my-5 w-80 h-auto rounded shadow-lg hover:shadow-xl hover:shadow-gray-300 duration-300'>
+        <div key={slug} className='my-5 p-3 w-72 h-auto border-2 border-slate-200 rounded-lg shadow-xl hover:shadow-xl hover:shadow-gray-300 duration-300 bg-gray-200'>
           <Link className='' href={`/posts/${slug}`}>
-          <Image className="w-full h-auto object-cover shadow-gray-100 shadow-md hover:shadow-xl duration-300 rounded-sm" src={metadata.thumbnail} alt='laptop' width={100} height={100} />
+          <Image className="w-full h-auto object-cover shadow-gray-100 shadow-md rounded-sm" src={metadata.thumbnail} alt='laptop' width={100} height={100} />
           <div className='p-5'>  
             <h2>{metadata.title}</h2>
             {metadata.description}
@@ -18,7 +18,7 @@ export default function PostsCard({postData, title}) {
           </div>
           </Link>
           <div className='p-5 pt-2'>
-            <Link className='bg-gray-300 p-2 hover:shadow-2xl duration-300 rounded-md font-medium' href={`/posts/category/${metadata.category}`}>
+            <Link className='bg-gray-300 p-2 hover:shadow-xl duration-300 rounded-md font-medium' href={`/posts/category/${metadata.category}`}>
               #{metadata.category}
             </Link>
           </div>
