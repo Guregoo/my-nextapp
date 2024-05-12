4880248b7af0839d891fbf100e30fd5711766e1a
Guregooo <michrio02@gmail.com>
2024-05-12 11:01:47 +0800

    adding get by category

--- pages/posts/category/[category].js
+++ pages/posts/category/[category].js
@@ -1,7 +1,41 @@
-export default function Category(){
-  return(
+import { useRouter } from 'next/router';
+import { getPostsByCategory, getAllCategories } from '@/lib/posts';
+import PostsCard from '@/components/PostsCard';
+
+export default function Category({ posts }) {
+  const router = useRouter();
+  const { category } = router.query;
+
+  if (!posts) {
+    return <div>Loading...</div>;
+  }
+
+  return (
     <div>
-      <h1>Ini category</h1>
+      <h1>Category : {category}</h1>
+      <PostsCard postData={posts} />
     </div>
-  )
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const category = params.category;
+  const posts = getPostsByCategory(category);
+  return {
+    props: {
+      posts
+    }
+  };
+}
+
+export async function getStaticPaths() {
+  const categories = getAllCategories(); // Mendapatkan semua kategori
+  const paths = categories.map(category => ({
+    params: { category }
+  }));
+
+  return {
+    paths,
+    fallback: false
+  };
 }
