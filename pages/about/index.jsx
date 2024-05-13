fcd18be5be83a02de830361ae0a53f0f1ebe6be9
Guregooo <michrio02@gmail.com>
2024-05-13 13:13:39 +0800

    fix about pages

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -9,7 +9,7 @@ export default function About(){
       </Head>
       <div className="flex flex-col justify-center text-center p-5">
         <ProfileCard />
-        <p className="p-4">Tak kenal maka tak sayang, maka dari itu Perkenalkan saya Gregorius Jovi, saya berasal dari kota Manado <br /> Saya seorang Mahasiswa <b>Prodi Teknik Informatika</b>, dalam dunia pemrograman saya masih seorang pemula.<br />Dunia pemrograman sangatlah luas karena rasa penasaran lah yang membuat saya mempelajari sesuatu</p>
+        <p className="p-4">Tak kenal maka tak sayang, maka dari itu Perkenalkan saya Gregorious Jovi, saya berasal dari kota Manado <br /> Saya seorang Mahasiswa <b>Prodi Teknik Informatika</b>, dalam dunia pemrograman saya masih seorang pemula.<br />Dunia pemrograman sangatlah luas karena rasa penasaran lah yang membuat saya mempelajari sesuatu</p>
       </div>
     </div>
   )
